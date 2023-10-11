ec8d39dd6c7e212d476aed3c78ddeabd2011d4d6
Somnath  More <somnath.more@zemosolabs.com>
2023-10-11 08:13:35 +0530

    Main changes

---
+++ Backend/sso-integration/src/test/java/com/sso/ssointegration/SsoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sso.ssointegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
