1413b9bdf7e5bfad3138ec8f6202db37b1f17973
Somnath  More <somnath.more@zemosolabs.com>
2023-10-11 08:17:45 +0530

    main file

---
+++ Backend/sso-integration/src/main/SsoIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.sso.ssointegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoIntegrationApplication.class, args);
+	}
+
+}
