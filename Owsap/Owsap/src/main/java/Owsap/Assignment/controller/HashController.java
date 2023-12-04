a91e87ac869588b6d615dedc4afbe753e53d72ce
Somnath  More <somnath.more@zemosolabs.com>
2023-12-04 12:38:29 +0530

    Owsap-Assignment SH1 algorithm Encryption

---
+++ Owsap/Owsap/src/main/java/Owsap/Assignment/controller/HashController.java
@@ -0,0 +1,28 @@
+package Owsap.Assignment.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@RestController
+@RequestMapping("/owsap")
+public class HashController {
+    @GetMapping("/hash")
+    public String hashString(@RequestParam String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] messageDigest = md.digest(input.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
